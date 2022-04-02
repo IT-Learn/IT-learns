1d994663cc5ac7fc148bc9029bfd4b227677a6dc
PRAKASH <87691043+IT-Learn@users.noreply.github.com>
2022-04-02 14:18:14 +0400

    Arithmetic operation using HTML and JS!!

---
+++ index.js
@@ -0,0 +1,6 @@
+ function a() {
+     var n1 = parseInt(document.getElementById("n1").value);
+     var n2 = parseInt(document.getElementById("n2").value);
+     var sum = n1+n2;
+     alert(sum);
+ }
